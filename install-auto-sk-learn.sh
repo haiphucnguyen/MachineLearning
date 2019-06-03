12687351a7ad62172f53353393066d6a90aa76e1
hainguyen <haiphucnguyen@gmail.com>
2019-06-03 17:18:45 -0500

    Update

--- install-auto-sk-learn.sh
+++ install-auto-sk-learn.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -p 8888:8888 --name jupyter -v $PWD/notes:/home/jovyan/work jupyter/tensorflow-notebook:abdb27a6dfbb
-
 docker exec -ti jupyter sh -c "conda install -y swig && pip install -r /home/jovyan/work/auto-sklearn-requirements.txt && pip install auto-sklearn"
