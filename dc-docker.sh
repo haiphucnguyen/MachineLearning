97b1f50d56ae240fc9fe25d617e24311cfe59608
hainguyen <haiphucnguyen@gmail.com>
2019-06-03 08:52:41 -0500

    Update

--- dc-docker.sh
+++ dc-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -p 8888:8888 -v $PWD/shared/notes:/home/jovyan/work jupyter/tensorflow-notebook:abdb27a6dfbb
+docker run -p 8888:8888 -v $PWD/notes:/home/jovyan/work jupyter/tensorflow-notebook:abdb27a6dfbb
