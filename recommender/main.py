0486a4f840f472c07e3118a6fb40cbabbfef8905
hainguyen <haiphucnguyen@gmail.com>
2019-06-04 16:31:03 -0500

    Update

--- recommender/main.py
+++ recommender/main.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 def hello():
     return "Oh, Hello World"
 
-if __name__ == "__main__":
-app.run(host='0.0.0.0', debug=True, port=80)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
