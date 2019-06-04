9e4b1660814a0a4fca72383dd3a48c6f4b41a2e2
wicknguyen <nguyenquy8321@gmail.com>
2019-06-03 23:46:28 -0500

    - Apply autoML (TPOT)

--- dc-docker.sh
+++ dc-docker.sh
@@ -2,4 +2,4 @@
 
 docker run -p 8888:8888 --name jupyter -v $PWD/notes:/home/jovyan/work jupyter/tensorflow-notebook:abdb27a6dfbb
 
-docker exec -ti jupyter sh -c "conda install -y swig && pip install -r /home/jovyan/work/auto-sklearn-requirements.txt && pip install auto-sklearn"
+docker exec -ti jupyter sh -c "conda install -y swig && pip install -r /home/jovyan/work/auto-sklearn-requirements.txt && pip install tpot"
--- install-auto-sk-learn.sh
+++ install-auto-sk-learn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -ti jupyter sh -c "conda install -y swig && pip install -r /home/jovyan/work/auto-sklearn-requirements.txt && pip install auto-sklearn"
+docker exec -ti jupyter sh -c "conda install -y swig && pip install -r /home/jovyan/work/auto-sklearn-requirements.txt && pip install tpot"
