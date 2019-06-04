df3b9b553319fa972f429c3ebd02e6794c8558e4
hainguyen <haiphucnguyen@gmail.com>
2019-06-03 20:45:52 -0500

    Update

---
+++ notes/test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import warnings;
+warnings.simplefilter('ignore')
+
+colNames = ['Age', 'WorkClass', 'FnlWgt', 'Education', 'Education-Num', 'Marital-Status', 'Occupation',
+            'Relationship', 'Race', 'Sex', 'Capital-Gain', 'Capital-Loss', 'Hours-Per-Week', 'Native-Country', 'Salary']
+dataset_train = pd.read_csv('data/adult-train.csv', header=None, names = colNames)
+dataset_train.head(10)
+
+dataset_test = pd.read_csv('data/adult-test.csv', header=None, names = colNames)
+dataset_test.head(10)
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
+# Check code https://www.kaggle.com/jeffd23/scikit-learn-ml-from-start-to-finish#Some-Final-Encoding to encode the
+# category values to numerical values
+
+
+labelEncoder = LabelEncoder()
+dataset_train = dataset_train.apply(labelEncoder.fit_transform)
+dataset_test = dataset_test.apply(labelEncoder.fit_transform)
+
+dropColumns = ['Salary', 'Education']
+X_train = dataset_train.drop(dropColumns, axis=1)
+Y_train = dataset_train['Salary']
+
+X_test = dataset_test.drop(dropColumns, axis=1)
+Y_test = dataset_test['Salary']
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+from autosklearn.classification import AutoSklearnClassifier
+
+cls = AutoSklearnClassifier(include_estimators=['lda','k_nearest_neighbors','passive_aggressive','gradient_boosting','extra_trees','liblinear_svc'],
+                              resampling_strategy='cv',
+                            resampling_strategy_arguments={'folds': 5}, ml_memory_limit=222000)
+cls.fit(X_train, Y_train)
+
+predictions = cls.predict(X_test)
