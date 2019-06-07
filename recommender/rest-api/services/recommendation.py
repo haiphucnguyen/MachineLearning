c1e136d53eaf9436ff442babc5920fad092df081
hainguyen <haiphucnguyen@gmail.com>
2019-06-07 11:03:22 -0500

    Update

--- recommender/rest-api/services/recommendation.py
+++ recommender/rest-api/services/recommendation.py
@@ -1,9 +1,13 @@
 from domain import MovieEntry
 from flask import jsonify
+from globals import Globals
 
 class RecommendationService:
 
     def execute(self, userid):
+
+        print(Globals.recommendObjs)
+
         data = [MovieEntry("1", "God Father", "https://m.media-amazon.com/images/M/MV5BMjQxM2YyNjMtZjUxYy00OGYyLTg0MmQtNGE2YzNjYmUyZTY1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg", 4),
                 MovieEntry("2", "God Father 2", "https://m.media-amazon.com/images/M/MV5BMjQxM2YyNjMtZjUxYy00OGYyLTg0MmQtNGE2YzNjYmUyZTY1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg", 5)]
         return jsonify([e.serialize() for e in data])
