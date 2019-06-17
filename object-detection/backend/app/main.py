968c91e3f817c8245308ebf839b80947cc568102
hainguyen <haiphucnguyen@gmail.com>
2019-06-16 19:45:12 -0500

    Update the docker file

--- object-detection/backend/app/main.py
+++ object-detection/backend/app/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import matplotlib.pyplot as plt, mpld3
 
 # the all-important app variable:
 app = Flask(__name__)
 
 @app.route("/")
-def listUsers():
-    return "Upload here"
+def index():
+    plt.plot([3, 1, 4, 1, 5], 'ks-', mec='w', mew=5, ms=20)
+    return mpld3.show()
 
 
 if __name__ == '__main__':
